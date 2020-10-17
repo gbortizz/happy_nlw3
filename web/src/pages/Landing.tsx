f29f31da552c13bc3702df5374c55d89f64a0319
Gabriele <54381611+gbortizz@users.noreply.github.com>
2020-10-17 15:49:37 -0300

    day 05 finished

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -18,8 +18,8 @@ function Landing() {
         </main>
 
         <div className="location">
-          <strong>Rio do Sul</strong>
-          <span>Santa Catarina</span>
+          <strong>Pelotas</strong>
+          <span>Rio grande do Sul</span>
         </div>
 
         <Link to="/app" className="enter-app">
